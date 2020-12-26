5711ba4901c0029b4a858270527fdf3841055003
sdkdeepa <deepa94086@gmail.com>
2020-12-25 19:54:20 -0800

    backend changes

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'st08c727a6';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-4mqcrvok.us.auth0.com',
+  clientId: 'IO00JzuHd3GR6ThValWi49PXuXupc4R9',
+  callbackUrl: 'http://localhost:3000/callback'
+};
