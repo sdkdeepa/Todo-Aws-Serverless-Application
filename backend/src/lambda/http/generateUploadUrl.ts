58d9acb6118b3c3ed86c51017982b7a555f10c1b
sdkdeepa <deepa94086@gmail.com>
2020-12-29 22:37:45 -0800

    Update generateUploadUrl.ts

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,45 +1,22 @@
 import 'source-map-support/register'
-import { APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda'
-import { createLogger } from '../../utils/logger';
-import { getUploadUrl} from '../../businessLogic/ToDo';
-import * as middy from 'middy';
-import { getUserId } from '../utils'
 
+import {APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler} from 'aws-lambda'
+import {generateUploadUrl} from "../../businessLogic/ToDo";
 
-const logger = createLogger('generateUploadUrl')
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+    console.log("Processing Event ", event);
+    const todoId = event.pathParameters.todoId;
 
-export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const URL = await generateUploadUrl(todoId);
 
-  logger.debug("Processing generateUploadUrl event", {event})
-
-  const todoId = event.pathParameters.todoId
-  console.log("Processing Event ", event);
-    const authorization = event.headers.Authorization;
-    const split = authorization.split(' ');
-    const jwtToken = split[1];
-
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  if (!todoId) {
     return {
-      statusCode: 400,
-      body: JSON.stringify({ error: 'todoId parameter is missing' })
-    }
-  }
-  const userId = getUserId(event)
-  logger.info("Getting signed URL for todo", {todoId, userId})
-
-  const signedUrl:string  = await getUploadUrl(todoId,jwtToken)
-  logger.info("Got signed URL for todo", {signedUrl})
-
-  // Return presigned url to client
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-       uploadUrl: signedUrl
-    }),
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-       'Access-Control-Allow-Credentials': true
-    }
-  } 
-})
+        statusCode: 202,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+            uploadUrl: URL,
+        })
+    };
+};
